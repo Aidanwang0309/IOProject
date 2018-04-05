9078aa9fb36cdee17c1bb48cf0de18020f99ebad
aidanwang0309 <aidanwang9303@gmail.com>
2018-04-05 15:16:23 -0400

    first commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,171 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    ofSetBackgroundAuto(false);
+    ofBackground(255);
+    
+    timeScale = 100.0;
+    clearAlpha = 0.5;
+    
+    prevx = prevy = degree = radian = x = y = 0.0f;
+    amp = 270; // size of the rose
+    x0 = y0 = 400; // this becomes the center
+    petals = 12; // number of petals
+    ofSetFrameRate(30);
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    // Reverse the contrast of the rectangles and screen over time
+    ofColor darkColor(0,0,0,255);  // Opaque black
+    ofColor lightColor(255,255,255,255);  // Opaque white
+    float time = ofGetElapsedTimef();  // Time in seconds
+    float percent = ofMap(cos(time/2.0), -1, 1, 0, 1);
+    ofColor bgColor = darkColor;
+    bgColor.lerp(lightColor, percent);
+    bgColor.a = clearAlpha;
+    ofColor fgColor = lightColor;
+    fgColor.lerp(darkColor, percent);
+    ofSetColor(bgColor);
+    
+    
+    ofSetRectMode(OF_RECTMODE_CORNER);
+    ofFill();
+    ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
+    
+    // Draw the spiraling rectangles
+    ofSetRectMode(OF_RECTMODE_CENTER);
+    ofSetColor(255);
+    ofNoFill();
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    for (int i=0; i<100; i++) {
+        ofScale(1.1, 1.1);
+        // Make the rectangles spiral
+        float time = ofGetElapsedTimef();
+        float noise = ofSignedNoise(time * timeScale) * 20.0;
+        ofRotate(noise);
+    
+        patternGenerating(6);
+    }
+    ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+    if( key=='r'){
+        //flip a boolean member variable
+        pdfRendering = !pdfRendering;
+        if( pdfRendering ){
+            //play around with this number to determine the optimal number of pages for your needs
+            ofSetFrameRate(12);  // so it doesn't generate tons of pages
+            //ofGetTimestampString is another timestamp variable and it's formatted nicely for filenames
+            ofBeginSaveScreenAsPDF("recording-"+ofGetTimestampString()+".pdf", true);
+        }else{
+            ofSetFrameRate(60);
+            ofEndSaveScreenAsPDF();
+        }
+    }
+}
+
+
+void ofApp::patternGenerating(int x){
+    
+    if(x==1){
+            ofSetPolyMode(OF_POLY_WINDING_NONZERO);
+            ofBeginShape();
+            ofVertex(10,35);
+            ofVertex(-85,35);
+            ofVertex(65,-75);
+            ofVertex(5,100);
+            ofVertex(-50,-75);
+            ofEndShape();
+    } else if (x==2){
+            radian = PI/180*(degree);
+            p = amp*cos(6*radian);
+            x = x0+p*cos(radian);
+            y = y0+p*sin(radian);
+            ofDrawLine(prevx, prevy, x, y);
+            prevx = x;
+            prevy = y;
+            degree++;
+    } else if (x==3){
+        ofDrawRectangle(0, 0, 50, 50);
+    } else if (x==4){
+        ofDrawTriangle(0, 20, 23, -20, -23, -20);
+    } else if (x==5){
+        ofDrawRectangle(-20, 20, 50, 50);
+        ofDrawRectangle(20, 20, 50, 50);
+        ofDrawRectangle(-20, -20, 50, 50);
+        ofDrawRectangle(20, -20, 50, 50);
+    }else if (x==6){
+        ofDrawEllipse(20, 20, 50, 50);
+        ofDrawEllipse(-20, -20, 50, 50);
+        ofDrawEllipse(20, -20, 50, 50);
+        ofDrawEllipse(-20, 20, 50, 50);
+    }
+    
+}
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    clearAlpha = ofMap(x, 0, ofGetWidth(), 0, 255);
+    timeScale = ofMap(y, 0, ofGetHeight(), 0, 1);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    void patternGenerating(int x);
+    bool pdfRendering = false;
+
+    float prevx, prevy, degree, radian, x, y, p;
+    int amp, x0,y0, petals;
+    int i=0;
+    int j = 0;
+    
+    float timeScale;
+    float clearAlpha;
+};
